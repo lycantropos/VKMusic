9cb971f4c0160378e14b7863db54134e868121a9
ace <azatibrakov@gmail.com>
2016-10-06 22:47:09 +0700

    Separated into two packages

--- manage.py
+++ manage.py
@@ -1,32 +1,8 @@
 import unittest
 
-from settings import APP_ID, USER_LOGIN, USER_PASSWORD, SCOPE, DST_ABSPATH, DATABASE_URL
-from tests.test_data_access import UnitTestDataAccess, UnitTestDataAccessExceptions
-from vk_music.app import MusicApp
-from vk_music.models import Base
-from vk_music.services.data_access import DataAccessObject
-
 import click
 
-
-@click.group(name='run', invoke_without_command=False)
-def run():
-    pass
-
-
-@run.command(name='init_db')
-def init_db():
-    """Invokes database according to models"""
-    data_access_object = DataAccessObject(DATABASE_URL)
-    Base.metadata.create_all(data_access_object.engine)
-
-
-@run.command(name='sync')
-def sync():
-    """Syncs VK audios with database and file system"""
-    music_app = MusicApp(APP_ID, USER_LOGIN, USER_PASSWORD, SCOPE)
-    path = DST_ABSPATH
-    music_app.synchronize_files(path)
+from tests.test_data_access import UnitTestDataAccess, UnitTestDataAccessExceptions
 
 
 @click.group(name='test', invoke_without_command=False)
@@ -43,7 +19,5 @@ def test_data_access():
     unittest.TextTestRunner(verbosity=2).run(exc_suite)
 
 
-manage = click.CommandCollection(sources=[run, test])
-
 if __name__ == '__main__':
-    manage()
+    test()
