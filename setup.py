a13cf260605dd41187115c659d78a7e3b3df5214
ace <azatibrakov@gmail.com>
2016-10-06 22:52:29 +0700

    'setup' module corrected: changed name of package to 'VKMusic'

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='VKApp',
+    name='VKMusic',
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/lycantropos/VKMusic',
     license='GNU GPL',
     author='lycantropos',
     author_email='azatibrakov@gmail.com',
-    description='Simple class for working with VK API',
+    description="Simple application for VK audio administrating",
     install_requires=[
         'SQLAlchemy==1.1.0', 'click==6.6'
     ],
